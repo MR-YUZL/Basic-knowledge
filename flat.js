acadaa260cf24202d627fc06c2139b2f68c56e42
yuzelin <932906404@qq.com>
2021-06-01 22:36:24 +0800

    flat

--- flat.js
+++ flat.js
@@ -41,18 +41,103 @@ const arr = [
 //   );
 // }
 
-function flat(arr) {
-  let arrRes = [];
-  let stack = [].concat(arr);
-  while (stack.length !== 0) {
-    let val = stack.pop();
-    if (Array.isArray(val)) {
-      stack.push(...val);
-    } else {
-      arrRes.push(val);
-    }
-  }
-  return arrRes;
-}
+// function flat(arr) {
+//   let arrRes = [];
+//   let stack = [].concat(arr);
+//   while (stack.length !== 0) {
+//     let val = stack.pop();
+//     if (Array.isArray(val)) {
+//       stack.push(...val);
+//     } else {
+//       arrRes.push(val);
+//     }
+//   }
+//   return arrRes;
+// }
 
-console.log(flat(arr));
+// function flat(arr, num = 1) {
+//   return num > 0
+//     ? arr.reduce(
+//         (per, cur) => per.concat(Array.isArray(cur) ? flat(cur, num - 1) : cur),
+//         []
+//       )
+//     : [].concat(arr);
+// }
+
+// Array.prototype.fakeFlat = function (num = 1) {
+//   if (!Number(num) || Number(num) < 0) {
+//     return this;
+//   }
+
+//   let arr = [].concat(this);
+
+//   while (num > 0) {
+//     if (arr.some((v) => Array.isArray(v))) {
+//       arr = [].concat.apply([], arr);
+//     } else {
+//       break;
+//     }
+//     num--;
+//   }
+
+//   return arr;
+// };
+
+// console.log(arr.fakeFlat(Infinity));
+
+let arr3 = arr.flat(0);
+arr3[0] = 10;
+console.log(arr3, arr);
+const arr2 = [1, [3, 4], , ,];
+
+// let arr4 = arr2;
+
+// arr4 = [2];
+// console.log(arr4, arr2);
+// let i = 0;
+// arr2.forEach((v) => i++);
+// console.log(i);
+
+// arr2.reduce((per, cur) => {
+//   console.log(cur);
+//   return per;
+// }, 0);
+
+Array.prototype.fakeFlat = function (num = 1) {
+  if (!Number(num) || Number(num) < 0) {
+    return [].concat(this);
+  }
+
+  let arr = [];
+
+  this.forEach((val) => {
+    if (Array.isArray(val)) {
+      arr = [].concat(val.fakeFlat(num - 1));
+    } else {
+      arr.push(val);
+    }
+  });
+
+  return arr;
+};
+
+// 大多数ES6方法会挑过空位
+// forEach(), filter(), reduce(), every() 和some() 都会跳过空位。
+
+let arr4 = arr2.map((v, index) => {
+  console.log("v", v, index);
+  return v
+});
+
+console.log(arr4)
+
+// join() 和 toString() 会将空位视为 undefined，而undefined 和 null 会被处理成空字符串。
+
+// ES6 明确将空位转为 undefined。
+
+// entries()、keys()、values()、find() 和 findIndex() 会将空位处理成 undefined。
+// for...of 循环会遍历空位。
+// fill() 会将空位视为正常的数组位置。
+// copyWithin() 会连空位一起拷贝。 会修改原数组  从数组的指定位置拷贝元素到数组的另一个指定位置中。
+// 扩展运算符（...）也会将空位转为 undefined。
+// Array.from 方法会将数组的空位，转为 undefined。
